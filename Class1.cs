00e64ad567159f9f77a22f5639093dc3c85f6081
minniunicorn <nknknkf92@gmail.com>
2022-02-17 09:07:24 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    ///<summary>
+    ///контрагенты
+    ///</summary>
+
+    internal class Company
+    {
+        ///<summary>
+        ///Название компании
+        ///</summary>
+        public string name;
+        ///<summary>
+        ///количество сотрудников
+        ///</summary>
+        public int persons;
+        ///<summary>
+        ///Зарплатный фонд
+        ///</summary>
+        public int money;
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Демонстариция работы класса");
+            Company MyCompany = new Company() { name = "Computerr",
+                                                persons = 10,
+                                                money = 300000};
+            Console.WriteLine(MyCompany.name + " " + MyCompany.persons + " " + MyCompany.money);
+            Console.ReadKey();
+        }
+    }   
+}
